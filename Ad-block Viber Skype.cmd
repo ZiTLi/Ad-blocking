8fa4ed6e36790343858ad5360e9dcc54979dbc11
ZiTLi <61345306+ZiTLi@users.noreply.github.com>
2020-02-22 10:23:27 +0200

    Update Ad-block Viber Skype.cmd

--- Ad-block Viber Skype.cmd
+++ Ad-block Viber Skype.cmd
@@ -1,3 +1,3 @@
-bitsadmin /transfer /download ТУТ %temp%\hosts & del %systemroot%\System32\drivers\etc\hosts_bak & ren %systemroot%\System32\drivers\etc\hosts hosts_bak & move %temp%\hosts %systemroot%\System32\drivers\etc
+bitsadmin /transfer /download https://raw.githubusercontent.com/ZiTLi/Ad-blocking/master/Block-ads %temp%\hosts & del %systemroot%\System32\drivers\etc\hosts_bak & ren %systemroot%\System32\drivers\etc\hosts hosts_bak & move %temp%\hosts %systemroot%\System32\drivers\etc
 
 
